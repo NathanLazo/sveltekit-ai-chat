cc1416f81b4c8da8e0c48dfd91155f84e65f860b
Nathan Lazo <nathanlazo@outlook.com>
2023-06-22 21:48:54 -0600

    v0.01

---
+++ src/routes/+page.svelte
@@ -0,0 +1,18 @@
+<script>
+import { useChat } from 'ai/svelte'
+
+const { messages, handleSubmit, input} = useChat({
+    api: '/chat',
+})
+</script>
+
+<ul>
+    {#each $messages as message}
+        <li>{message}</li>
+    {/each}
+</ul>
+
+<form on:submit={handleSubmit}>
+    <input bind:value={$input} />
+    <button type="submit">Send</button>
+</form>
